1bd8fbbed3507839c6fb7d0dfd2b4a553fbc2373
Blake Vanier <blakevanier@gmail.com>
2018-04-04 10:13:34 -0600

    added new method

---
+++ dayOfYear.java
@@ -0,0 +1,27 @@
+public static int dayOfYear(int month, int dayOfMonth, int year) {
+    if (month == 2) {
+        dayOfMonth += 31;
+    } else if (month == 3) {
+      
+      
+      
+      
+      sss
+        ss
+        s
+        s
+        s
+        ss
+        s
+        s
+        
+        
+        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+    } else if (month == 11) {
+        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+    } else if (month == 12) {
+        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31;
+    }
+    return dayOfMonth;
+}
+
